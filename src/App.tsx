346747141e58991fb363d2397fca95b61c8f9561
0me1ettetuy <Omelettetuy@gmail.com>
2026-08-26 23:27:59 +0500

    tanstack query client + provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-function App() {
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
+
+function App() {
   return (
     <>
-      app
+      <QueryClientProvider client={queryClient}>App</QueryClientProvider>
     </>
   );
 }
